dc488bea469c3e03d767c7a5cdcbb528ebdf8a5e
KobiDouglasCook <kobidouglascook@gmail.com>
2025-07-28 16:19:04 -0400

    finished deploy script

---
+++ scripts/prod/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+BUCKET_NAME="fuego-socks-prod-templates"
+STACK_NAME="fuego-socks-prod"
+PARAM_FILE="parameters/prod.json"
+
+echo ">>> Creating S3 bucket..."
+aws s3 mb s3://$BUCKET_NAME || echo "Bucket already exists, continuing..."
+
+echo ">>> Packaging CloudFormation templates..."
+aws cloudformation package \
+  --template-file root-stack.yaml \
+  --s3-bucket $BUCKET_NAME \
+  --output-template-file root-stack-packaged.yaml
+
+echo ">>> Deploying CloudFormation stack..."
+aws cloudformation deploy \
+  --template-file root-stack-packaged.yaml \
+  --stack-name $STACK_NAME \
+  --parameter-overrides file://$PARAM_FILE \
+  --capabilities CAPABILITY_NAMED_IAM
